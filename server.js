e4e3fe579c93741f159e2e134deddee8960e599c
The Python Controller <express.vanukuri@gmail.com>
2025-08-27 23:25:22 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
@@ -7,9 +8,9 @@ const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 8080;
 
-const SPOTIFY_CLIENT_ID = "";
-const SPOTIFY_CLIENT_SECRET = "";
-const SPOTIFY_REDIRECT_URI = "http://127.0.0.1:8080/";
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_REDIRECT_URI = process.env.SPOTIFY_REDIRECT_URI;
 
 let spotifyTokens = {};
 
